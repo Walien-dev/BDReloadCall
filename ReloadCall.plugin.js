b5d39cf38dd3ffc6dd86f10b5155f754c51c3262
Walien_ <43910644+Walien-dev@users.noreply.github.com>
2021-06-06 21:17:37 +0200

    Update ReloadCall.plugin.js

--- ReloadCall.plugin.js
+++ ReloadCall.plugin.js
@@ -181,7 +181,7 @@ var ReloadCall = (_ => {
 					if (elms[i].innerHTML == this.channel) {
 						var cadenas = elms[i].parentElement.parentElement.querySelector('.icon-1DeIlz').getElementsByTagName('path')[0].getAttribute('d')
 						if (cadenas != LockedChannel) {
-							document.querySelector('[aria-label="Déconnexion"]').click()
+							document.querySelector('[d="'+DecoButton+'"]').parentElement.parentElement.parentElement.click()
 							console.log("Connexion en cours au channel [" + this.channel + "]")
 							elms[i].click()
 						} else {
@@ -199,3 +199,4 @@ var ReloadCall = (_ => {
 
 
 var LockedChannel = "M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z"
+var DecoButton = "M21.1169 1.11603L22.8839 2.88403L19.7679 6.00003L22.8839 9.11603L21.1169 10.884L17.9999 7.76803L14.8839 10.884L13.1169 9.11603L16.2329 6.00003L13.1169 2.88403L14.8839 1.11603L17.9999 4.23203L21.1169 1.11603ZM18 22H13C6.925 22 2 17.075 2 11V6C2 5.447 2.448 5 3 5H7C7.553 5 8 5.447 8 6V10C8 10.553 7.553 11 7 11H6C6.063 14.938 9 18 13 18V17C13 16.447 13.447 16 14 16H18C18.553 16 19 16.447 19 17V21C19 21.553 18.553 22 18 22Z"
